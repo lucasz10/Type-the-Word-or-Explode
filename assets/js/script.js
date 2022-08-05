e053bf5becc19cf086b9ecbc9ab5698fdbef41e3
lucasz10 <lucas.zach10@gmail.com>
2022-08-04 21:33:02 -0700

    Cleaned up branch to work with my edits.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,21 @@
 //Section for defining global variables
 
-var timerEl = document.getElementById('gameBrief');
+var startTimerEl = document.getElementById('startTimer');
 // var mainEl = document.getElementById('Time');
 // change or adder h2 header to add id = time
 var message = "10 words"
 var startBtn = document.getElementById("nicknameBtn");
+var endBtn = document.getElementById("returnHome");
 
+var debugBtn = document.getElementById("debugBtn");
+
+function countdown() {
+  var timeLeft = 10
+  var name = getElementById('nickName')
 
 //Section for defining functions
 
-function countdown() {
-    var timeLeft = 10
-    var name = getElementById('nickName')
-
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + ' seconds remaining' + name ;
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + ' second remaining' + name ;
-        timeLeft--;
-      } else {
-
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-        displayMessage();
-        console.log(name)
-
-        //maybe look at timer ending in seperate function
-
-      }
-    }, 1000);
-}
   
-function displayMessage() { //Displaying the challenge words? Work off this code to display words. Don't need timer
-    var wordCount = 0;
-  
-    var msgInterval = setInterval(function () {
-
-      if (words[wordCount] === undefined) {
-        clearInterval(msgInterval);
-      } else {
-        mainEl.textContent = words[wordCount];
-        wordCount++;
-      }
-    }, 3000);
- }
-
 function callWordsAPI() {
 
   var wordsAPI = "https://random-words-api.herokuapp.com/w?n=10"; //Need to throw this call in beginning of gameStart function
@@ -62,6 +31,7 @@ function callWordsAPI() {
 
 function gameStart(words) {
   
+  var startTimeLeft = 5;
   var guessWordsObj = []; //Creates a multidimensional array of guesswords with strings broken out into separate arrays
 
   for(i = 0; i < words.length ; i++) {
@@ -75,12 +45,34 @@ function gameStart(words) {
   document.getElementById("inGame").style.display = "none";
   document.getElementById("endGame").style.display = "none";
 
+  document.getElementById("gameBrief").style.display = "flex";
+
+  var timeInterval = setInterval(function () {
+    
+    if (startTimeLeft > -1) {
+      startTimerEl.textContent = "Time to start: " + startTimeLeft ;
+      startTimeLeft--;
+    } else {
+      startTimerEl.textContent = '';  
+      clearInterval(timeInterval);  
+      typingGame(guessWordsObj);        
+    }
+  }, 1000);
 
 };
 
+function typingGame(gameObjective) {
+  
+  document.getElementById("gameBrief").style.display = "none";
+  document.getElementById("inGame").style.display = "flex";
 
-
-
+  for(i=0 ; i < gameObjective.length ; i++) {
+    $("#guessingWords").innerHTML = gameObjective[i][0]
+    // $('#debugBtn').click(function() {
+    //   i = i + 1;
+    // })
+  }
+};
 //Function that starts the game, sets display of landingPage to none
 //Function should include timer, when ending criteria is met, set display of gameBrief to none
 
@@ -88,9 +80,41 @@ function gameStart(words) {
 
 //Function for storing gameData locally
 
+const saveState = {
+  charName: 'nickName',
+  results: 'endResults',
+  words: "words",
+};
+
+const saveStateString = JSON.stringify(saveState);
+localStorage.setItem('saveState', saveStateString);
+
+const loadSaveStateString = localStorage.getItem('saveState');
+const loadSaveState = JSON.parse(loadSaveStateString);
+
 //Function for calling Giphy api, may be able to include in active game function
 
+let APIKEY = "ssS6qrhl6fvn3W4QEtQrYHXKJmdrveFI";
+
+function gify(state) {
+
+
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=` + state;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (content) {
+      console.log(content);
+      document.getElementById("test1").src = content.data[0].images.downsized.url;
+    });
+
+}
+gify('sweating')
+//Add eventListeners for return button and start button
 
 //Event Listeners will be listed below
 
 startBtn.addEventListener("click", callWordsAPI);
+endBtn.addEventListener("click", returnHome);
